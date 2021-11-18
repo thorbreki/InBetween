4334021b88ac5c4ceaa9330b989c0dc42b5b6772
thorbreki <thorbreki@gmail.com>
2021-11-19 00:43:24 +0100

    Stompers, Projectilers, Basics are all working pretty well now and the Enemy Spawner acts not like a survival mode!

--- InBetweenGame/Assets/Scripts/EnemyScripts/EnemySpawnController.cs
+++ InBetweenGame/Assets/Scripts/EnemyScripts/EnemySpawnController.cs
@@ -9,24 +9,79 @@ public class EnemySpawnController : MonoBehaviour
     [Header("Enemy objects")]
     [SerializeField] private GameObject basicEnemyObject;
     [SerializeField] private GameObject projectileEnemyObject;
+    [SerializeField] private GameObject stomperEnemyObject;
 
+    [Header("Enemy Spawning Attributes")]
+    [SerializeField] private float spawnYLevel; // Where on the y-axis will the enemies spawn
+    [SerializeField] private float minXSpawn; // What is the minimum on the x-axis enemies can spawn
+    [SerializeField] private float maxXSpawn; // What is the maximum on the x-axis enemies can spawn
+    [SerializeField] private float beginningSpawningCooldown; // How fast does the spawner spawn enemies in the beginning of survival mode
+
+    /// <summary>
+    /// When the spawner speeds up, how much does it speed up
+    /// </summary>
+    [SerializeField] private float spawningSpeedUp; // When the spawner speeds up, how much does it speed up
+
+    /// <summary>
+    /// In seconds, how much time needed for the enemy spawner to speed up
+    /// </summary>
+    [SerializeField] private float speedUpCoolDownSec; // In seconds, how much time needed for the enemy spawner to speed up
+
+    private float spawnCooldown;
     private Vector3 spawnPosition;
 
 
     private void Start()
     {
-        spawnPosition = new Vector3(5, 5, 0);
-        StartCoroutine(spawnEnemies());
+        spawnPosition = new Vector3(Random.Range(minXSpawn, maxXSpawn), spawnYLevel, transform.position.z);
+        spawnCooldown = beginningSpawningCooldown;
+        StartCoroutine(spawnEnemies()); // Start spawning enemies!
+        StartCoroutine(IncreaseSpawningCooldown()); // Start speeding up!
     }
 
     // A coroutine that spawns enemies
     private IEnumerator spawnEnemies()
     {
-        while (!Input.GetKeyDown(KeyCode.Escape))
+        while (!Input.GetKey(KeyCode.Escape))
         {
-            yield return new WaitForSeconds(3f);
-            GameObject newEnemy = Instantiate(basicEnemyObject, spawnPosition, Quaternion.identity);
-            newEnemy.GetComponent<BasicEnemyController>().playerTransform = playerTransform;
+            int randomEnemyNumber = Random.Range(1, 4); // The number which will decide which enemy type gets spawned
+            GameObject newEnemy;
+
+            switch (randomEnemyNumber)
+            {
+                case 1:
+                    newEnemy = SpawnEnemy(basicEnemyObject);
+                    newEnemy.GetComponent<BasicEnemyController>().playerTransform = playerTransform;
+                    break;
+                case 2:
+                    newEnemy = SpawnEnemy(projectileEnemyObject);
+                    newEnemy.GetComponent<ProjectileEnemyController>().SetPlayerTransform(playerTransform);
+                    break;
+                default: // When the value is 3
+                    newEnemy = SpawnEnemy(stomperEnemyObject);
+                    newEnemy.GetComponent<StomperController>().playerTransform = playerTransform;
+                    break;
+            }
+            yield return new WaitForSeconds(spawnCooldown);
         }
+    }
+
+    private GameObject SpawnEnemy(GameObject newEnemyObject)
+    {
+        return Instantiate(newEnemyObject, spawnPosition, Quaternion.identity);
+    }
+
+    /// <summary>
+    /// This coroutine decreases the spawning cooldown according to the speedUpCooldownSec variable
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator IncreaseSpawningCooldown()
+    {
+        while (!Input.GetKey(KeyCode.Escape))
+        {
+            yield return new WaitForSeconds(speedUpCoolDownSec);
+            spawnCooldown -= spawningSpeedUp; // Actually speed up the spawner
+        }
+
     }
 }
--- InBetweenGame/Assets/Scripts/EnemyScripts/StomperController.cs
+++ InBetweenGame/Assets/Scripts/EnemyScripts/StomperController.cs
@@ -10,57 +10,92 @@ public class StomperController : MonoBehaviour
     [SerializeField] private float minMovementSpeed;
     [SerializeField] private float maxMovementSpeed;
 
+    [SerializeField] private float minDesiredX = -24f;
+    [SerializeField] private float maxDesiredX = 24f;
+    [SerializeField] private float minDesiredY = 4f;
+    [SerializeField] private float maxDesiredY = 14f;
+
+    [SerializeField] private float minAttackForce; // The minimum speed the enemy goes down to kill
+    [SerializeField] private float maxAttackForce; // The maximum speed the enemy goes down to kill
+
     private float movementSpeed;
+    private float attackForce;
 
-    private int positionIndex; // The index into the stomperPositions array in GameManager
+    private Vector2 desiredDirection;
+    private Vector2 attackVector;
 
-    private Vector3 desiredSpot;
-
+    private Coroutine moveCor;
     private Coroutine attackPlayerCor;
 
     private void Start()
     {
-        // TODO: REMOVE THE FUCKING CODE YOU JUST WROTE AND MAKE THEM ROAM AROND FREELY LIKE TOTAL ASSHATS, MUCH EASIER CODE, AND TAKES LESS TIME TO IMPLEMENT
-        desiredSpot = new Vector3(0, 0, transform.position.z);
+        desiredDirection = Vector2.one;
 
         movementSpeed = Random.Range(minMovementSpeed, maxMovementSpeed);
-        // Find the target spot this enemy wants to go to
-        FindDesiredSpot();
+        attackForce = Random.Range(minAttackForce, maxAttackForce);
+        attackVector = new Vector2(0, -attackForce);
+
+        moveCor = StartCoroutine(MoveToDesiredPos());
     }
 
     private void Update()
     {
-        MoveToDesiredSpot();
-    }
+        if (playerTransform == null) { return; }
 
-    private void FindDesiredSpot()
-    {
-        (int targetXPosition, int targetYPosition, int newIndex) = GameManager.instance.GetRandomStomperPosition();
-        positionIndex = newIndex; // Now the enemy knows where the index of its position is in the stomperPositions List
-
-        if (positionIndex == -1) // If no position was found
+        if (Mathf.Abs(playerTransform.position.x - transform.position.x) <= 0.99f)
         {
-            desiredSpot.x = transform.position.x;
-            desiredSpot.y = transform.position.y;
-        } else
-        {
-            desiredSpot.x = targetXPosition;
-            desiredSpot.y = targetYPosition;
+            if (attackPlayerCor != null) { return; } // Make sure to not activate the attack coroutine again and again
+            if (moveCor != null) { StopCoroutine(moveCor); moveCor = null; } // Make sure that the enemy is not trying to get to its desired spot anymore
+            attackPlayerCor = StartCoroutine(AttackPlayer()); // Start the attack coroutine
         }
     }
 
-    private void MoveToDesiredSpot()
+    private IEnumerator MoveToDesiredPos()
     {
-        rigidBody.velocity = (desiredSpot - transform.position).normalized * movementSpeed;
+        attackPlayerCor = null; // This might be confusing, but this ensures that the enemy can attack again, since not sure if StopCoroutine makes coroutines == null
+        while (true)
+        {
+            // Decide on a random direction
+            print("Choosing a new direction!");
+            desiredDirection.x = Random.Range(-5, 5);
+            desiredDirection.y = Random.Range(-5, 5);
+            rigidBody.velocity = desiredDirection.normalized * movementSpeed;
+            yield return new WaitForSeconds(Random.Range(0.5f, 5f));
+        }
     }
 
-    private void AttackPlayer()
+    private IEnumerator AttackPlayer()
     {
+        yield return new WaitForSeconds(0.1f);
+        desiredDirection.x = 0;
+        desiredDirection.y = 0;
+        rigidBody.velocity = desiredDirection;
 
+        rigidBody.AddForce(attackVector, ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(3f);
+        moveCor = StartCoroutine(MoveToDesiredPos());
     }
 
-    private void OnDestroy()
-    {
-        GameManager.instance.FreeUpStomperPosition(positionIndex); // Free up the position for other stompers
-    }
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    print("TRIGGERED!");
+    //    if (!other.CompareTag("StomperBorder")) { return; } // if this enemy is not colliding with bottom stomper border, then return
+
+    //    if (moveCor == null) { return; } // If this enemy is not moving (aka attacking) then we will not care
+
+    //    desiredDirection.y = 5f; // Make the enemy want to go up
+    //    rigidBody.velocity = desiredDirection.normalized * movementSpeed;
+
+    //}
+
+    //private void OnTriggerStay2D(Collider2D other)
+    //{
+    //    if (!other.CompareTag("StomperBorder")) { return; } // if this enemy is not colliding with bottom stomper border, then return
+
+    //    if (moveCor == null) { return; } // If this enemy is not moving (aka attacking) then we will not care
+
+    //    desiredDirection.y = 5f; // Make the enemy want to go up
+
+    //}
 }
